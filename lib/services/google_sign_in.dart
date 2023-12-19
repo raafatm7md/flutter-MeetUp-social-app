72b504f35cc26c3c412bb050b77ad378396fc4a3
Raafat Elmenayar <raafat.el123@gmail.com>
2023-12-19 23:59:21 +0200

    some updates

--- lib/services/google_sign_in.dart
+++ lib/services/google_sign_in.dart
@@ -1,7 +1,9 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInService{
-  static final _googleSignIn = GoogleSignIn();
+  static final _googleSignIn = GoogleSignIn(
+      clientId: '203347747842-2uq462l0kh21istb4db60mm87os3tark.apps.googleusercontent.com'
+  );
 
   static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
 
